fa2fe6272673e0eac76e5aa5438a1d5a76848278
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-08-09 12:54:28 -0500

    Se renombró la variable de la factoria

--- creation/abstract-factory/labyrinth/main.ts
+++ creation/abstract-factory/labyrinth/main.ts
@@ -3,22 +3,22 @@ import { LabyrinthFactory } from './factories/labyrinth-factory'
 import { NormalLabyrinthFactory } from './factories/normal-labyrinth-factory'
 
 export class LabyrinthGame {
-  constructor(private labyrinths_factory: LabyrinthFactory) {}
+  constructor(private labyrinth_factory: LabyrinthFactory) {}
 
   create_labyrinth(): Labyrinth {
-    const labyrinth = this.labyrinths_factory.make_labyrinth()
-    const room1 = this.labyrinths_factory.make_room(1)
-    const room2 = this.labyrinths_factory.make_room(1)
-    const door = this.labyrinths_factory.make_door(room1, room2)
+    const labyrinth = this.labyrinth_factory.make_labyrinth()
+    const room1 = this.labyrinth_factory.make_room(1)
+    const room2 = this.labyrinth_factory.make_room(1)
+    const door = this.labyrinth_factory.make_door(room1, room2)
 
-    room1.set_side('north', this.labyrinths_factory.make_wall())
+    room1.set_side('north', this.labyrinth_factory.make_wall())
     room1.set_side('east', door)
-    room1.set_side('surth', this.labyrinths_factory.make_wall())
-    room1.set_side('west', this.labyrinths_factory.make_wall())
+    room1.set_side('surth', this.labyrinth_factory.make_wall())
+    room1.set_side('west', this.labyrinth_factory.make_wall())
 
-    room2.set_side('north', this.labyrinths_factory.make_wall())
-    room2.set_side('east', this.labyrinths_factory.make_wall())
-    room2.set_side('surth', this.labyrinths_factory.make_wall())
+    room2.set_side('north', this.labyrinth_factory.make_wall())
+    room2.set_side('east', this.labyrinth_factory.make_wall())
+    room2.set_side('surth', this.labyrinth_factory.make_wall())
     room2.set_side('west', door)
 
     labyrinth.add_room(room1)
